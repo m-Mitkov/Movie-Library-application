631b4e6dc63f8892607a517e50f302c9d9b242d3
Martin Mitkov <56538513+m-Mitkov@users.noreply.github.com>
2021-05-31 22:33:47 +0300

    Added logout functionality in Navbar Component

--- front-end/src/components/Navbar/Navbar.js
+++ front-end/src/components/Navbar/Navbar.js
@@ -1,18 +1,24 @@
-import { useRef } from 'react';
+import { useRef, useContext } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 
 import style from './Navbar.module.css';
 import { Button } from '@material-ui/core'
-import SeacrhPage from '../SearchPage/SearchPage';
+import { BASE_URL, LOGOUT } from '../../consts/endPointsAPI';
+import { fetchServiceGET } from '../../services/fetchService';
+import { Context } from '../../Store/Store';
+import { SUCCESS_NOTIFICATION, ERROR_NOTIFICATION, LOGOUT_SUCCESS } from '../../actions/types';
 
 const Navbar = () => {
+
+    const { auth, notification } = useContext(Context);
+    const [user, authDispatch] = auth;
+    const [notify, notifyDispatch] = notification;
 
     const history = useHistory();
     const inputRef = useRef();
 
     const onClickHandler = () => {
         const searchParam = inputRef.current.value;
-        console.log('click');
 
         history.push({
             pathname: `/search/${searchParam}`,
@@ -20,11 +26,37 @@ const Navbar = () => {
         })
     }
 
+    const handleLogout = () => {
+        fetchServiceGET(BASE_URL + LOGOUT, '', '')
+        .then( (res) => {
+
+            authDispatch({type: LOGOUT_SUCCESS});
+            notifyDispatch({type: SUCCESS_NOTIFICATION, payload: {message: res}})
+            history.push('/');
+
+        })
+        .catch(err => {
+            notifyDispatch({type: ERROR_NOTIFICATION, payload: {message: 'Please try again!'}})
+        });
+    }
+
     return (
         <div className={style.navbar}>
             <Link to='/'>
                 <span className={style.title}>My Movie Collection</span>
             </Link>
+
+            {
+                user.username
+                    ?
+                    <div className={style.logout}> 
+                        <Button variant="outlined" color="primary"
+                        onClick={handleLogout}>
+                        Logout</Button>
+                    </div>
+
+                    : ''
+            }
 
             <form className={style.inputForm}>
 
@@ -38,6 +70,7 @@ const Navbar = () => {
                     onClick={onClickHandler}
                 >
                     Search</Button>
+
 
             </form>
         </div>
